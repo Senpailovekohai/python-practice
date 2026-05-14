0d861bf4b160994832b2f33caaa032d3c0efc552
Rahul Thapa <rahulmagar180@gmail.com>
2026-05-14 21:31:48 +0900

    First Python practice

---
+++ main.py
@@ -0,0 +1,11 @@
+mark=int(input("Enter your mark?"))
+if mark>=90:
+    print("You got A+")
+elif mark>=80:
+    print("You got A")
+elif mark>=70:
+    print("You got B+")
+elif mark>=60:
+    print("You got B")
+else:
+    print("Try Again")
