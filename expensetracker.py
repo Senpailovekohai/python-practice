f5f5c0db3d15eacb9e25891357b43a01f325ab98
Rahul Thapa <rahulmagar180@gmail.com>
2026-06-14 11:57:33 +0900

    Expense tracker Using Python

--- expensetracker.py
+++ expensetracker.py
@@ -1,39 +1,83 @@
-expense=[]
-condition="yes"
-totalamount=0
-while condition =="yes":
-    name=input("Enter expenses name:")
-    amount=int(input("Enter amount of expense:"))
-    cost={
+import json
+import os
+if os.path.exists("expenses.json"):
+   with open("expenses.json","r") as file:
+      expense=json.load(file)
+else:
+   expense=[]
+   
+
+while True:
+    print("1.Add Expense" )
+    print("2.Delete Expense")
+    print("3. View Specifiy Expense")
+    print("4.View Detail Calculation")
+    print("5.Exit")
+   
+    choice=input("Enter your choice:")  
+    if choice=="1":
+        condition="yes"
+        while condition =="yes":
+            name=input("Enter expenses name:").lower().strip()
+            while True:
+                try:
+                    amount=int(input("Enter amount of expense:"))
+                    break
+                except:
+                    print("Please only Input Enter Number:")
+            cost={
         "name":name,
         "amount":amount
         
-    }
-    expense.append(cost)
-    condition=input("Do you want to add another expenses?").lower()
-    totalamount=totalamount+amount
+            }
+            expense.append(cost)
+            condition=input("Do you want to add another expenses?").lower()
+            while condition !="yes" and condition!="no":
+                print("Please Enter Yes/No")
+                condition=input("Do you want to add another expenses?").lower()
+
+    elif choice=="2":
+        delete=input("Which expense you wanna delete:").lower().strip()
+        for de in expense:
+          if de["name"]==delete:
+             expense.remove(de)
+             print("Sucessfull Deleted!!")
+             break
+        else:
+            print("Expense Name not Found!!")
+
+    elif choice=="3":
+        exp=input("Which expense you wanna see?").lower().strip()
+        for cost in expense:
+           if cost["name"]==exp:
+               print(
+                    cost["name"],
+                     "-",
+                    cost["amount"]
+               )
+               break
+        else:
+           print("Expense name not found!!")
+               
     
-print("Your total amount is:",totalamount)
-
-
-for expenses in expense:
-  print( expenses["name"],
-        "-",
-        expenses["amount"]
+           
        
-      
-      
-  )
+    elif choice=="4":
+        total_amount=0
+        for cost in expense:
+            print(
+          cost["name"],
+          "-",
+          cost["amount"]
+       )
+            total_amount=total_amount+cost["amount"]
+        print("Your total amount is:",total_amount)
+            
+    elif choice=="5":
+        break
 
 
-file=open("expenses.txt","w")
 
-for item in expense:
-   file.write(
-      item["name"]
-      +"-"
-      +str(item["amount"])
-      +"won\n"
-   )
-file.write("Total-"+str(totalamount)+"won")
-file.close()
+   
+with open("expenses.json","w") as file:
+    json.dump(expense, file)
--- main.py
+++ main.py
@@ -1,39 +1,9 @@
-student=[]
-condition="yes"
-while condition=="yes":
-    name=input("Enter student name:")
-    while True:
-        try:
-            id=int(input("Enter student id"))
-            break
-        except:
-            print("Error!!Please! Enter number only")
+expense = [{"name": "latte", "amount": 500}]
 
-    dep=input("Enter the department of the student:")
-
-    detail={ "name":name,
-            "student id":id,
-            "department":dep
-    }
-    student.append(detail)
-    condition=input("Do you want to add another ?").lower()
-    while condition!="yes" and condition!="no":
-        print("Error!! Please type only Yes/No")
-        condition=input("Do you want to add another?").lower()
-
-for info in student:
-    print(info["name"],
-          "-",
-          "Student id:",
-          info["student id"],
-          "-",
-          "Department:",
-          info["department"]
-
-
-    )
-
-
-
-
-
+exp = "rr"
+for cost in expense:
+    if cost["name"] == exp:
+        print("found")
+        break
+else:
+    print("not found")
